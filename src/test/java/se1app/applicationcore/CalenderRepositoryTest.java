e05ffbf6dcdd8035574861d1b24194427317f340
CasaSky <talal.tabia.one@gmail.com>
2015-12-07 19:53:14 +0100

    Alle Tests

--- src/test/java/se1app/applicationcore/CalenderRepositoryTest.java
+++ src/test/java/se1app/applicationcore/CalenderRepositoryTest.java
@@ -1,7 +1,91 @@
 package se1app.applicationcore;
 
+import org.assertj.core.api.StrictAssertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import se1app.applicationcore.complexDataTyp.Email;
+import se1app.applicationcore.complexDataTyp.Gender;
+import se1app.applicationcore.repositories.CalenderRepository;
+import se1app.applicationcore.repositories.ClientRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by talal on 06.12.15.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = Application.class)
 public class CalenderRepositoryTest {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private CalenderRepository calenderRepository;
+
+
+    @Before
+    public void setup() {
+        Client talal = new Client("Talal", Gender.MALE, 25, Email.getEmail("talal.tabi@gmail.com"));
+        talal.addDestination(new Destination("Casablanca", "Summer", new Date(2016, 06, 01), 20, "Flugzeug"));
+        clientRepository.save(talal);
+
+        Calender talalCalender = new Calender(new Date(2016, 06, 02), "Sehr gute Bewertung von anderen Reisenden");
+        talal.addCalender(talalCalender);
+        calenderRepository.save(talalCalender);
+
+
+        Client kyo = new Client("Kyo", Gender.MALE, 26, Email.getEmail("lee@googlemail.com"));
+        kyo.addDestination(new Destination("Seoul", "Summer", new Date(2016, 8, 21), 10, "Auto"));
+        clientRepository.save(kyo);
+
+        Calender kyoCalender = new Calender((new Date(2016, 12, 24)), "Diese Reise schenke ich mir für meinen Geburtstag");
+        kyo.addCalender(kyoCalender);
+
+        calenderRepository.save(kyoCalender);
+
+
+        Client superman = new Client("supermarkt", Gender.MALE, 35, Email.getEmail("super@superemail.com"));
+        superman.addDestination(new Destination("Mars", "Winter", new Date(2222, 9, 22), 100, "Spaceship"));
+        clientRepository.save(superman);
+
+        Calender supermanCalender = new Calender((new Date(2016, 7, 24)), "Fuer die Welt");
+        superman.addCalender(supermanCalender);
+
+        calenderRepository.save(supermanCalender);
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Calender> calender = calenderRepository.findAll();
+        assertThat(calender).hasSize(3);
+    }
+
+    @Test
+    public void testFindbyTripDate(){
+        //Optional<Client> client = clientRepository.findByClientName("kyo");
+        Optional<Calender> calenderNotice = calenderRepository.findByTripDate((new Date(2016, 7, 24)));
+        StrictAssertions.assertThat(calenderNotice.isPresent());
+        StrictAssertions.assertThat(calenderNotice.get().getTripDetails().equals("Fuer die Welt"));
+    }
+
+    /*@Test
+    public void testFindbyClient(){;
+        Optional<Client> kyo = clientRepository.findByClientName("Kyo");
+        StrictAssertions.assertThat(kyo.isPresent());
+        Optional<Calender> calender = calenderRepository.findByClient(kyo.get());
+        StrictAssertions.assertThat(calender.isPresent());
+        StrictAssertions.assertThat(calender.get().getTripDate().equals((new Date(2016, 8, 21))));
+    }*/
 }
--- src/test/java/se1app/applicationcore/DestinationRepositoryTest.java
+++ src/test/java/se1app/applicationcore/DestinationRepositoryTest.java
@@ -1,7 +1,78 @@
 package se1app.applicationcore;
 
+import org.assertj.core.api.StrictAssertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import se1app.applicationcore.complexDataTyp.Email;
+import se1app.applicationcore.complexDataTyp.Gender;
+import se1app.applicationcore.repositories.CalenderRepository;
+import se1app.applicationcore.repositories.ClientRepository;
+import se1app.applicationcore.repositories.DestinationRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by talal on 06.12.15.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = Application.class)
 public class DestinationRepositoryTest {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private DestinationRepository destinationRepository;
+
+
+    @Before
+    public void setup() {
+        Client talal = new Client("Talal", Gender.MALE, 25, Email.getEmail("talal.tabi@gmail.com"));
+        talal.addDestination(new Destination("Casablanca", "Summer", new Date(2016, 06, 01), 20, "Flugzeug"));
+        clientRepository.save(talal);
+
+        Client kyo = new Client("Kyo", Gender.MALE, 26, Email.getEmail("lee@googlemail.com"));
+        kyo.addDestination(new Destination("Seoul", "Summer", new Date(2016, 8, 21), 10, "Auto"));
+        kyo.addDestination(new Destination("Tokyo", "Herbst", new Date(2016, 9, 30), 9, "Schiff"));
+        clientRepository.save(kyo);
+
+        Client superman = new Client("supermarkt", Gender.MALE, 35, Email.getEmail("super@superemail.com"));
+        superman.addDestination(new Destination("Mars", "Winter", new Date(2222, 9, 22), 100, "Spaceship"));
+        clientRepository.save(superman);
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Destination> destination = destinationRepository.findAll();
+        assertThat(destination).hasSize(4);
+    }
+
+    @Test
+    public void testFindbyDate(){
+        //Optional<Client> client = clientRepository.findByClientName("kyo");
+        List<Destination> destinations = destinationRepository.findByDate(new Date(2016, 8, 21));
+        StrictAssertions.assertThat(!destinations.isEmpty());
+        StrictAssertions.assertThat(destinations.contains("Seoul"));
+    }
+
+    @Test
+    public void testFindbyClient(){;
+        Optional<Client> kyo = clientRepository.findByClientName("Kyo");
+        StrictAssertions.assertThat(kyo.isPresent());
+        List<Destination> destination = destinationRepository.findByClients(kyo.get());
+        StrictAssertions.assertThat(!destination.isEmpty());
+        StrictAssertions.assertThat(destination.contains("Seoul"));
+        StrictAssertions.assertThat(destination.contains("Tokyo"));
+    }
 }
